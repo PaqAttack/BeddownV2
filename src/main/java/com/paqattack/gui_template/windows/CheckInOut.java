794d7b7d5305d420729e5ce82f56104c8c77a68f
cpaquin <cpaquin966@gmail.com>
2023-11-19 22:10:47 +0900

    Many optimizations in code

--- src/main/java/com/paqattack/gui_template/windows/CheckInOut.java
+++ src/main/java/com/paqattack/gui_template/windows/CheckInOut.java
@@ -2,6 +2,7 @@ package com.paqattack.gui_template.windows;
 
 import com.paqattack.gui_template.Session;
 import com.paqattack.gui_template.WindowManager;
+import com.paqattack.gui_template.data.Employee;
 import com.paqattack.gui_template.data.ListEntry;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -10,14 +11,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import org.joda.time.DateTime;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CheckInOut extends AnchorPane implements Updatable {
     WindowManager windowManager;
     private static final Logger logger = Logger.getLogger(CheckInOut.class.getName());
-
     @FXML
     TextField scanBox;
     @FXML
@@ -26,6 +29,8 @@ public class CheckInOut extends AnchorPane implements Updatable {
     Button clearScanBtn;
     @FXML
     ListView<ListEntry> entryListView;
+    private Timer timer = new Timer();
+    private boolean timerActive = false;
 
     public CheckInOut(WindowManager windowManager) {
         this.windowManager = windowManager;
@@ -44,6 +49,31 @@ public class CheckInOut extends AnchorPane implements Updatable {
 
         backPane.setOnMouseMoved(event -> scanBox.requestFocus());
         clearScanBtn.setOnAction(event -> scanBox.clear());
+
+    }
+
+    private void extendTimer() {
+
+    }
+
+    public void onChange() {
+
+    }
+
+    public void scannedChange() {
+        if (scanBox.getText().length() == 99) {
+            String scannedID = scanBox.getText();
+
+            if (scannedEmployee != null) {
+                session.createListEntry(scannedEmployee, ListEntry.convertDateToString(new DateTime()));
+                updateAccountabilityInterfaceElements();
+                System.out.println("Created list entry");
+            } else {
+                // fill blocks with si data
+                transferSIData(si);
+            }
+            scantxt.setText("");
+        }
     }
 
     @Override
